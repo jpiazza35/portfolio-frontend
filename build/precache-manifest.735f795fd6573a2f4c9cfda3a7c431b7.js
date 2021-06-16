3634c5614c95f45a55ad42abf55da00a2d4866bc
Jose ignacio Piazza <piazzajignacio@gmail.com>
2021-06-16 18:15:15 -0300

    modify files

---
+++ build/precache-manifest.735f795fd6573a2f4c9cfda3a7c431b7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16279a18f1d8d042188348fc2f2f48f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52a6140ac43056e245d5",
+    "url": "/static/css/main.5b0f3f49.chunk.css"
+  },
+  {
+    "revision": "780e81b42988d6ee931e",
+    "url": "/static/js/2.9853ba99.chunk.js"
+  },
+  {
+    "revision": "2206f6f6cbbf99cae08d01b2952f71ad",
+    "url": "/static/js/2.9853ba99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52a6140ac43056e245d5",
+    "url": "/static/js/main.36922cdc.chunk.js"
+  },
+  {
+    "revision": "c4f7cb2ed6ce8d3a24b8",
+    "url": "/static/js/runtime-main.04827dba.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "963722b14500b5ddaf8fb9c06511dcda",
+    "url": "/static/media/avatar.963722b1.png"
+  },
+  {
+    "revision": "9cdcd0538164271aba91f4c7871626c2",
+    "url": "/static/media/introBG.9cdcd053.jpg"
+  },
+  {
+    "revision": "9f803b78a1f809757e6a8c53271813a4",
+    "url": "/static/media/portfolioBG.9f803b78.jpeg"
+  }
+]);
